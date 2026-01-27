4d3ea78320c8c68405a0ae2fa62b951684e5740a
Guilherme Alves Ostolim Totoli <gtotoli@MacBook-Pro-M2.local>
2026-01-26 23:28:38 -0300

    Chore: Bump version and force unique build ID to invalidate Netlify cache

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,9 @@
 const nextConfig = {
     // Enable if using simple export, otherwise standard build works for Vercel/Node
     // output: 'export', 
+    generateBuildId: async () => {
+        return `build-${Date.now()}`
+    },
 };
 
 export default nextConfig;
